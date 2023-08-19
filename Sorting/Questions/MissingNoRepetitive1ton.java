1501af45f4079a24a1cd795540004c44fe13a0af
Mohor Banerjee <115227638+MoBanerjee@users.noreply.github.com>
2023-08-19 09:12:20 +0800

    Add files via upload

---
+++ Sorting/Questions/MissingNoRepetitive1ton.java
@@ -0,0 +1,42 @@
+package Algorithms;
+
+import java.util.*;
+
+public class MissingNoRepetitive1ton {
+	static List<Integer> missingno2(int[]arr) {
+		ArrayList<Integer>result=new ArrayList<>();
+		int i=0;
+		int n=arr.length;
+		while(i<arr.length) {
+		 if(arr[i]!=i+1 && arr[arr[i]-1]!=arr[i]) {
+				int temp=arr[arr[i]-1];
+				
+				arr[arr[i]-1]=arr[i];
+				arr[i]=temp;
+			}
+			else {i++;}
+		}
+		i=0;
+
+		
+		while(i<arr.length) {
+			if(arr[i]==i+1) {
+				i++;
+			}
+			else {
+				result.add(i+1);
+			
+				i++;
+			}
+		}
+		return result ;
+	}
+	public static void main(String[] args) {
+		int[]arr= {3,1,3,5,5,7,6};
+		int[]arr2= {4,4,4,4};
+		System.out.println(missingno2(arr));
+		System.out.println(missingno2(arr2));
+	}
+
+
+}
