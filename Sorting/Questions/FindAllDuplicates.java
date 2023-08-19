d7eae08284e79ac94c2b6f5f53a655bf081f69e2
Mohor Banerjee <115227638+MoBanerjee@users.noreply.github.com>
2023-08-19 10:17:20 +0800

    Add files via upload

---
+++ Sorting/Questions/FindAllDuplicates.java
@@ -0,0 +1,27 @@
+package Questions;
+import java.util.*;
+public class FindAllDuplicates {
+	static List<Integer> findallduplicates(int[]arr) {
+		ArrayList<Integer> result=new ArrayList<>();
+		int i=0;
+		while(i<arr.length) {
+			if(arr[i]!=i+1) {if(arr[arr[i]-1]!=arr[i]) {
+				int temp=arr[arr[i]-1];
+				
+				arr[arr[i]-1]=arr[i];
+				arr[i]=temp;
+			}else{
+				result.add(arr[i]);
+				i++;
+				}
+			}
+			else {i++;}
+		}
+		return result;
+	}
+public static void main(String[] args) {
+	int[]arr= {1,1,4,4,5,7,5};
+	System.out.println(findallduplicates(arr));
+}
+
+}
